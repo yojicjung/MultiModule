9f3e7b95dab3ea2c470b797e2c8fdff6398252da
YOJIC-J <dywlr74@hmcnetworks.co.kr>
2023-12-22 12:33:53 +0900

    전체 파일 업로드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.6"
-    id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("jvm") version "1.9.20"
+    kotlin("kapt") version "1.9.20"
 }
 
 group = "com.hmcnetworks"
@@ -14,14 +12,17 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
@@ -29,12 +30,4 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.bootBuildImage {
-    builder.set("paketobuildpacks/builder-jammy-base:latest")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "yojic"
+
+include("app-core")
+include("common")
+include("db-access")
+include("domain")
+include("rest-api")
